6ddd5fb06e15f0c82dbf35545be1973f5d32a7db
BassantWael816 <bassantwael181@gmail.com>
2026-01-26 21:59:57 +0200

    Add project files.

---
+++ IjarifySystemDAL/Data/Context/IjarifyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IjarifySystemDAL.Data.Context
+{
+    public class IjarifyDbContext : DbContext
+    {
+        public IjarifyDbContext(DbContextOptions<IjarifyDbContext> options) : base(options)
+        {
+        }
+    }
+}
