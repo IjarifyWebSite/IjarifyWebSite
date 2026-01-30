9ead161849998532aa8c11c653507194b49e096a
BassantWael816 <bassantwael181@gmail.com>
2026-01-30 15:47:41 +0200

    Add Review Structure to BLL

---
+++ IjarifySystemBLL/Services/Classes/ReviewService.cs
@@ -0,0 +1,20 @@
+﻿using IjarifySystemBLL.Services.Interfaces;
+using IjarifySystemDAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IjarifySystemBLL.Services.Classes
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly IReviewRepository reviewRepository;
+
+        public ReviewService(IReviewRepository reviewRepository)
+        {
+            this.reviewRepository = reviewRepository;
+        }
+    }
+}
