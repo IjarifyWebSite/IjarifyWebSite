2c97f6dcba2c3c48d6a137a4d207f117d5fd1bc0
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-02-04 22:47:36 +0200

    Feat/Add LocationOffersPageViewModel and LocationRepository

---
+++ IjarifySystemBLL/ViewModels/LocationsViewModel/LocationOffersPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using IjarifySystemBLL.ViewModels.OfferViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IjarifySystemBLL.ViewModels.LocationsViewModel
+{
+    public class LocationOffersPageViewModel
+    {
+        public string City { get; set; } = null!;
+        public string LocationImageUrl { get; set; } = null!;
+        public List<OfferViewModel> Offers { get; set; } = null!;
+    }
+}
