e55bf1fd5956e7278055c5d26b8d273604b90e79
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-02-04 17:07:57 +0200

    Feat/Add HomeService and IHomeService for location retrieval

---
+++ IjarifySystemBLL/Services/Interfaces/IHomeService.cs
@@ -0,0 +1,14 @@
+﻿using IjarifySystemBLL.ViewModels.LocationsViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IjarifySystemBLL.Services.Interfaces
+{
+    public interface IHomeService
+    {
+        IEnumerable<LocationViewModel> GetTopLocations();
+    }
+}
