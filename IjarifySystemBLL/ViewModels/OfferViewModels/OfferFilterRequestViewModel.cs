5f5dd1e0d7ec087c3ed9c10db96eaf206c945bd2
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-02-06 02:07:42 +0200

    Feat/ Add View Models For Filtering Offers Page

---
+++ IjarifySystemBLL/ViewModels/OfferViewModels/OfferFilterRequestViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IjarifySystemBLL.ViewModels.OfferViewModels
+{
+    public class OfferFilterRequestViewModel
+    {
+        public string? SearchTerm { get; set; }
+        public List<decimal>? MinimumDiscount { get; set; }
+        public List<string>? Areas { get; set; }
+        public List<string>? Compounds { get; set; }
+
+    }
+}
