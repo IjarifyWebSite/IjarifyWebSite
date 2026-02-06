fcab816b2a921f365fbbbf2d87c0658220e0f8d4
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-02-06 02:11:22 +0200

    Feat/ add view and controller for offer filtering

--- IjarifySystemPL/Controllers/OfferController.cs
+++ IjarifySystemPL/Controllers/OfferController.cs
@@ -1,6 +1,6 @@
 ﻿using IjarifySystemBLL.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-
+using IjarifySystemBLL.ViewModels.OfferViewModels;
 namespace IjarifySystemPL.Controllers
 {
     public class OfferController : Controller
@@ -17,8 +17,19 @@ namespace IjarifySystemPL.Controllers
         }
         public IActionResult LocationOffers(string City)
         {
-            var LocationOffersPage= _offerService.GetAllOffersByLocation(City);
+            var LocationOffersPage = _offerService.GetAllOffersByLocation(City);
             return View(LocationOffersPage);
         }
-    }
+        public IActionResult FilterOffers()
+        {
+            var FilterPage = _offerService.GetFilterPageIntialData();
+            return View(FilterPage);
+        }
+        [HttpGet]
+        public IActionResult FilterOffers(OfferFilterRequestViewModel Filter)
+        {
+            var FilteredOffers = _offerService.GetFilteredOffers(Filter);
+            return View(FilteredOffers);
+        }
+    } 
 }
