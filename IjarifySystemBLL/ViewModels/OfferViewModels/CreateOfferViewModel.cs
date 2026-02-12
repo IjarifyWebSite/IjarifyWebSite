0be32d0562974c8d41f894b5e9511955bb22c39e
AhmedTawfikkk <ahmedmohamed2042003@gmail.com>
2026-02-12 03:58:44 +0200

    Feat/ Add owner-specific offer views and Handle CRUD views

--- IjarifySystemBLL/ViewModels/OfferViewModels/CreateOfferViewModel.cs
+++ IjarifySystemBLL/ViewModels/OfferViewModels/CreateOfferViewModel.cs
@@ -11,6 +11,7 @@ namespace IjarifySystemBLL.ViewModels.OfferViewModels
 {
     public class CreateOfferViewModel
     {
+        public int Id { get; set; }
         [Required(ErrorMessage ="Title is required.")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 100 characters.")]
         public string Title { get; set; } = null!;
